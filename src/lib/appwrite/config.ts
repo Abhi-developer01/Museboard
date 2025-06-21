d2280ce5f11f03bc8cb9e854b6a7eaadae719367
Abhi-developer01 <theabhishek96@gmail.com>
2025-06-21 18:03:52 +0530

    revert

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-  url: '/api/appwrite',
+  url: import.meta.env.VITE_APPWRITE_URL,
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
